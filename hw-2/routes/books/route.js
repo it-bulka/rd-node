7c75d236fbbb240dd2690acb641af49e5760017a
IT.bulka <i.it.bulka@gmail.com>
2025-06-23 17:08:03 +0200

    hw2/add validation for title and author fields

--- hw-2/routes/books/route.js
+++ hw-2/routes/books/route.js
@@ -1,13 +1,28 @@
 import { createBook, getAllBooks } from "../../services/books.service.js"
 
 export const POST = async (req, res) => {
-  const { title, author }  = req.body
+  const { title, author } = req.body
   if (!title || !author) {
     return res.answer(404, {
       success: false,
-      error: "Both Title and Author fields should be provided"
+      error: "Both title and author fields should be provided"
     })
   }
+
+  if(typeof title !== 'string') {
+    return res.answer(404, {
+      success: false,
+      error: "Field title should be a string type"
+    })
+  }
+
+  if(typeof author !== 'string') {
+    return res.answer(404, {
+      success: false,
+      error: "Field author should be a string type"
+    })
+  }
+
   const book = await createBook(req.body)
   return res.answer(201, {
     success: false,
