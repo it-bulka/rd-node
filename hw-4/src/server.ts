2342d4255cbf53c8710db4a55cf63831c285180a
IT.bulka <i.it.bulka@gmail.com>
2025-07-03 20:39:55 +0200

    hw-4-express/add graceful shutdown while SIGTERM/SIGINT

--- hw-4/src/server.ts
+++ hw-4/src/server.ts
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 import http from 'node:http'
 import { createApp } from './app.ts'
+import { container } from '@/container.ts'
 
 const app = await createApp()
 const server = http.createServer(app)
@@ -11,3 +12,18 @@ const port = process.env.PORT || 3000
 server.listen(port, () => {
   console.log(`Server started on port ${port}`)
 })
+
+function shutDown() {
+  console.log('Shutting down gracefully...')
+  server.close(async () => {
+
+    await container.dispose()
+
+    console.log('Closed out connections')
+    process.exit(0)
+  })
+
+  setTimeout(() => process.exit(1), 10_000).unref()
+}
+process.on('SIGTERM', shutDown)
+process.on('SIGINT',  shutDown)
