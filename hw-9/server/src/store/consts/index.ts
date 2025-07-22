a00876f3b8c4fe098df7ddea982737f40f728fa1
IT.bulka <i.it.bulka@gmail.com>
2025-07-22 18:11:46 +0200

    hw-9-websocket: level up dir for db

--- hw-9/server/src/store/consts/index.ts
+++ hw-9/server/src/store/consts/index.ts
@@ -1,6 +1,6 @@
 import  * as path from 'path'
 
-export const DB_DIR_PATH = path.resolve(__dirname, '..', 'db')
+export const DB_DIR_PATH = path.resolve(process.cwd(), '..', 'db')
 
 export const MODELS_PATHS = {
   users: path.resolve(DB_DIR_PATH, 'users.json'),
